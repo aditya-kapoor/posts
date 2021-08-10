901d9f024a5f9cdaf54d980772b41c97779a8ffe
Aditya Kapoor <adityakapoor.mait@gmail.com>
2021-08-11 03:11:58 +0530

    Generate graphiti resources for posts

---
+++ app/resources/post_resource.rb
@@ -0,0 +1,15 @@
+class PostResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :published_at, :datetime
+  attribute :user_id, :integer
+  attribute :title, :string
+  attribute :html, :string
+  attribute :status, :integer_enum, allow: Post.statuses.keys
+
+  # Direct associations
+
+  # Indirect associations
+
+end
