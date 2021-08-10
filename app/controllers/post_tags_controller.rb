7a2ada04c037d722073734951454e97834c22ad6
Aditya Kapoor <adityakapoor.mait@gmail.com>
2021-08-11 03:11:37 +0530

    Display associated collections on show pages

--- app/controllers/post_tags_controller.rb
+++ app/controllers/post_tags_controller.rb
@@ -42,8 +42,14 @@ class PostTagsController < ApplicationController
   # DELETE /post_tags/1
   def destroy
     @post_tag.destroy
-    redirect_to post_tags_url, notice: 'Post tag was successfully destroyed.'
+    message = "PostTag was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to post_tags_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
